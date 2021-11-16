e73c90efe4877c70756de2a26ed337b322741577
Jwger <fz0429@126.com>
2021-11-16 18:14:58 +0800

    无更新

---
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -0,0 +1,36 @@
+package com.nowcoder.community.config;
+
+import com.nowcoder.community.controller.interceptor.AlphaInterceptor;
+import com.nowcoder.community.controller.interceptor.LoginTicketInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig  implements WebMvcConfigurer {
+
+    @Autowired
+    private AlphaInterceptor alphaInterceptor;
+
+    @Autowired
+    private LoginTicketInterceptor loginTicketInterceptor;
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //拦截器
+        registry.addInterceptor(alphaInterceptor)
+                //哪些资源可以不用拦截
+                //这些静态资源都不需要拦截
+                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.jpg","/**/*.png","/**/*.jpeg")
+                //指定拦截哪些资源
+                .addPathPatterns("/register","/login");
+
+        registry.addInterceptor(loginTicketInterceptor)
+                //哪些资源可以不用拦截
+                //这些静态资源都不需要拦截
+                .excludePathPatterns("/**/*.css","/**/*.js","/**/*.jpg","/**/*.png","/**/*.jpeg");
+
+    }
+
+
+}
