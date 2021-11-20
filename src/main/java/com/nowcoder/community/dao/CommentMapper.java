a5ff9418490e61ed5147dd5ae171cf7fd5d0b434
Jwger <fz0429@126.com>
2021-11-20 21:31:22 +0800

    评论功能

---
+++ src/main/java/com/nowcoder/community/dao/CommentMapper.java
@@ -0,0 +1,18 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.Comment;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentMapper {
+
+    //查询某一页有多少数据
+    List<Comment> selectCommentsByEntity(int entityType, int entityId, int offset, int limit);
+
+    //查询某一页有多少条数据
+    int selectCountByEntity(int entityType, int entityId);
+
+    int insertComment(Comment comment);
+}
