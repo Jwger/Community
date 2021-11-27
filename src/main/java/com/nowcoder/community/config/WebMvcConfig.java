92c4b566aa6fdd0cdb60373e021ae43e1016f711
Jwger <fz0429@126.com>
2021-11-27 12:22:16 +0800

    消息管理，消息通知，系统通知，拦截器，拦截数量

--- src/main/java/com/nowcoder/community/config/WebMvcConfig.java
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.nowcoder.community.config;
 import com.nowcoder.community.controller.interceptor.AlphaInterceptor;
 import com.nowcoder.community.controller.interceptor.LoginRequiredInterceptor;
 import com.nowcoder.community.controller.interceptor.LoginTicketInterceptor;
+import com.nowcoder.community.controller.interceptor.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -18,6 +19,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
     private LoginRequiredInterceptor loginRequiredInterceptor;
     @Autowired
     private LoginTicketInterceptor loginTicketInterceptor;
+    @Autowired
+    private MessageInterceptor messageInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -39,5 +42,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addInterceptor(loginRequiredInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js",
                         "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+        registry.addInterceptor(messageInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js",
+                        "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
+
     }
 }
--- src/main/java/com/nowcoder/community/controller/interceptor/LoginTicketInterceptor.java
+++ src/main/java/com/nowcoder/community/controller/interceptor/LoginTicketInterceptor.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Date;
